63a9f2a075ae8c8628705f2bec6a25966054de94
shaan <shaanjassal101@gmail.com>
2018-10-25 22:47:42 +0100

    Added 'TransferCards' function. Incomplete.
    Signed-off-by: shaan <shaanjassal101@gmail.com>

--- DistributeCards.py
+++ DistributeCards.py
@@ -2,7 +2,7 @@
 def DisCards(ListOfCards):
 	# Extra safety check
 	if len(ListOfCards) % 2 == 0:
-		list1 = ListOfCards[:len(ListOfCards)/2]
-		list2 = ListOfCards[len(ListOfCards)/2:]
+		list1 = ListOfCards[:int(len(ListOfCards)/2)]
+		list2 = ListOfCards[int(len(ListOfCards)/2):]
 		return list1, list2
 	
--- PlayHand.py
+++ PlayHand.py
@@ -15,4 +15,29 @@ def PlayHand(card1, card2, category):
         else:
             winner = card2
         return winner 
+
+def TransferCards(list1,list2,category):
+    card1 = list1[0]
+    card2 = list2[0]
+    winner = PlayHand(card1,card2,category)
+    if winner == card1:
+        # list1 has won this hand.
+        # winning card moves from front to back of list1.
+        # losing card is removed from list2 and placed at back of list1.
+        list1.remove(card1)
+        list1.append(card1)
+        list2.remove(card2)
+        list1.append(card2)
+    else:
+        # list2 has won this hand.
+        # winning card moves from front to back of list2.
+        # losing card is removed from list1 and placed at back of list2.
+        list2.remove(card2)
+        list2.append(card2)
+        list1.remove(card1)
+        list2.append(card1)
+     # for debug
+    return list1,list2
+        
+        
     
