1362bbde5d69aa56c81db77e73c0f643cc44ac4e
Shaan <shaanjassal101@gmail.com>
2018-11-06 20:40:30 +0000

    Called PlayHand until we find a winner - unfinished
    Signed-off-by: Shaan <shaanjassal101@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import CreateCards
 import DistributeCards
+import PlayHand 
 NumOfCards = 24
-
 
 cards = CreateCards.CreCards(NumOfCards)
 list1,list2 = DistributeCards.DisCards(cards)
-print(list1,list2)
 
+while len(list1)!= 0 and len(list2)!= 0:
+    list1,list2 = PlayHand.PlayHand(list1,list2,"size")
+if len(list1) > len(list2):
+    print("winner = list1")
+else:
+    print("winner = list2")
