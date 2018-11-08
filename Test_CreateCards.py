8b4538e888531d5d81d94ee3863e685f21550562
Shaan <shaanjassal101@gmail.com>
2018-11-08 20:30:35 +0000

    Moved files out of UnitTests into main folder
    Signed-off-by: Shaan <shaanjassal101@gmail.com>

--- Test_CreateCards.py
+++ Test_CreateCards.py
@@ -1,19 +1,13 @@
-
-import sys, os
-
-sys.path.append('../')   THIS DOES NOT WORK
-
 import CreateCards
-
-
 
 f = open("TestFile_CreateCards.txt","r")
 lines = f.readlines()
 for num in lines:
+    num = int(num) 
     cards = CreateCards.CreCards(num)
     # ToDo check for neg num!!
     #if num < 0:
-        
+      
     if num == len(cards):
         print("yay, card num:", num)
     else:
