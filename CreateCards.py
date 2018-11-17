dce805473efefef706559c7c90b99ff81825ba0e
Shaan <shaanjassal101@gmail.com>
2018-11-17 18:50:52 +0000

    Basic game completed, lots of refinement needed
    Signed-off-by: Shaan <shaanjassal101@gmail.com>

--- CreateCards.py
+++ CreateCards.py
@@ -2,9 +2,9 @@
 #from dogs.txt and generating random values for the rest. Stores these dictionaries in  a list.
 #input: number of cards made (as dictionaries)
 #output: list of these cards
-AttrTypes = ["friendliness", "excercise", "drool", "size"]
+
 import random
-def CreCards(NumOfCards):
+def CreCards(NumOfCards, Categories):
 	ListOfCards = []
 	f = open("dogs.txt","r")
 	ListOfNames = f.readlines()
@@ -13,10 +13,10 @@ def CreCards(NumOfCards):
 			card = {}
 			card['name'] = ListOfNames[x]
 			random.seed()  
-			card[AttrTypes[0]] = random.randint(0,10)
-			card[AttrTypes[1]] = random.randint(0,10)
-			card[AttrTypes[2]] = random.randint(0,5)
-			card[AttrTypes[3]] = random.randint(0,10)
+			card[Categories[0]] = random.randint(0,10)
+			card[Categories[1]] = random.randint(0,10)
+			card[Categories[2]] = random.randint(0,5)
+			card[Categories[3]] = random.randint(0,10)
 			ListOfCards.append(card)
 	return ListOfCards
         
